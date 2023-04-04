3b6c052a147b760955eb7c2221327fef0779fc82
genaro-cravero <nicocraveroo@gmail.com>
2023-04-04 19:54:24 -0300

    Json in progress...

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+let monitorsContainter = document.querySelector(".monitorsContainer");
+
+async function fetchProducts() {
+    try{
+        const response = await fetch("./products.json");
+        const data = await response.json();
+        return data;
+    }catch(error){
+        console.log(error);
+    }
+}
+async function getProducts(category, container){
+    const products = await fetchProducts();
+    for(product of products[category]){
+
+        container.innerHTML += 
+        `
+        <div class="monitor">
+        <img src="${product.image}" alt="Monitor">
+        <h3>${product.name}</h3>
+        <p>$ ${product.price}</p>
+        </div> 
+        `
+    }
+}
+getProducts("monitors", monitorsContainter);
+
