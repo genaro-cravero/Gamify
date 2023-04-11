58e2d24b9dbc613bb387222becbc5d3031f68982
genaro-cravero <nicocraveroo@gmail.com>
2023-04-11 16:00:14 -0300

    Js self content adjust | some content in json

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let monitorsContainter = document.querySelector(".monitorsContainer");
-
 async function fetchProducts() {
     try{
         const response = await fetch("./products.json");
@@ -9,20 +7,33 @@ async function fetchProducts() {
         console.log(error);
     }
 }
-async function getProducts(category, container){
-    const products = await fetchProducts();
-    for(product of products[category]){
 
-        container.innerHTML += 
-        `
-        <div class="card">
-            <img src="${product.image}" alt=${product.alt}>
-            <h3>${product.name}</h3>
-            <p>$ ${product.price}</p>
-            <button class="btn">Comprar</button>
-        </div> 
-        `
-    }
+////Obtain products from JSON 
+
+async function getProducts(){
+    const products = await fetchProducts();
+    let categories = Object.keys(products);
+
+    //? for each category, create a container and add the products
+    categories.forEach((category, index) =>{
+
+        let container = document.querySelector(`.${category}Container`);
+
+        //? for each product, create a card and add it to the container
+        for(product of products[category]){
+
+            container.innerHTML += 
+            `
+            <div class="card">
+                <img src="${product.image}" alt=${product.alt}>
+                <h3>${product.name}</h3>
+                <p>$ ${product.price}</p>
+                <button class="btn">Comprar</button>
+            </div> 
+            `
+        }
+    });
+    
 }
-getProducts("monitors", monitorsContainter);
+getProducts();
 
